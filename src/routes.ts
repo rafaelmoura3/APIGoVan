5d38f2e49dd641e0aae8f372de63311d69ea0258
Rafael Moura <rafinha.rp9@gmail.com>
2021-07-28 19:18:03 -0300

    routes

---
+++ src/routes.ts
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+import tipoUsuarioController from './controllers/tipoUsuarioController';
+
+const routes = Router();
+
+routes.post('/tipousuario', tipoUsuarioController.create);
+    
+   
+
+
+export default routes;
--- src/servers.ts
+++ src/servers.ts
@@ -3,9 +3,11 @@ import express, { request, response } from 'express';
 
 
 import './database/connection';
+import routes from './routes';
 
 const app = express();
 app.use(express.json());
+app.use(routes);
 
 //rota = conjunto de instruções
 //recuso = usuario
